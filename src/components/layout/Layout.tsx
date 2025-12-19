cc185f07c09dcf420c565c7b2fa2e93b4f8c3001
eliza-stack <bknade794@gmail.com>
2025-12-19 15:04:24 +0300

    first commit

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,12 +3,16 @@ import Contact from "../sections/contact/Contact";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
 import scss from "./Layout.module.scss";
+import FAQ from "../sections/faq/faq";
+import Hero from "../sections/hero/Hero";
 
 const Layout = () => {
   return (
     <div className={scss.layout}>
       <Header />
       <main className="main">
+        <Hero />
+        <FAQ />
         <Contact />
       </main>
       <Footer />
