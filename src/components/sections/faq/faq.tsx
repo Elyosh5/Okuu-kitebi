4676bd1ec0d73a16d0509daf77dc3f84af92c0df
Zhigitekova Nazik <rayanajung123@gmail.com>
2025-12-23 15:33:15 +0600

    Fix faq section styles and component

--- src/components/sections/faq/faq.tsx
+++ src/components/sections/faq/faq.tsx
@@ -1,74 +1,65 @@
 "use client";
-
-import React, { useState } from "react";
-import Image from "next/image";
-import scss from "./faq.module.scss";
-import plusIcon from "../../../assets/sections/openIcon.png";
-import closeIcon from "../../../assets/sections/closeIcon.png";
+import React from "react";
+import { Collapse } from "antd";
+import style from "./Faq.module.scss";
+import { PlusCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 
 interface FaqItem {
+  id: number;
   question: string;
   answer: string;
 }
 
+const label: string =
+  "Lorem ipsum dolor sit amet consectetur. Nisi orci vulputate nisl viverra sit congue";
+const paragraph: string =
+  "Lorem ipsum dolor sit amet consectetur. Nisi orci vulputate nisl viverra sit congue.Id quis sit lobortis amet in et.";
+
+const faqItems: FaqItem[] = [
+  { id: 1, question: label, answer: paragraph },
+  { id: 2, question: label, answer: paragraph },
+  { id: 3, question: label, answer: paragraph },
+  { id: 4, question: label, answer: paragraph },
+  { id: 5, question: label, answer: paragraph },
+];
+
 const Faq: React.FC = () => {
-  const [openIndex, setOpenIndex] = useState<number | null>(null);
-
-  const label =
-    "Lorem ipsum dolor sit amet consectetur. Nisi orci vulputate nisl viverra sit congue";
-  const text =
-    "Lorem ipsum dolor sit amet consectetur. Nisi orci vulputate nisl viverra sit congue. Id quis sit lobortis amet in et. At nibh proin non ut vulputate id amet eu massa. Lorem ipsum dolor sit amet consectetur. Nisi orci vulputate nisl viverra sit";
-
-  const faqItems: FaqItem[] = [
-    { question: label, answer: text },
-    { question: label, answer: text },
-    { question: label, answer: text },
-  ];
-
-  const toggle = (index: number) => {
-    setOpenIndex((prev) => (prev === index ? null : index));
+  const panelStyle: React.CSSProperties = {
+    minHeight: "80px",
+    marginBottom: 20,
+    background: "#F2F2F7",
+    borderRadius: 16,
+    border: "1px solid #F2F2F7",
+    padding: "10px 16px",
   };
 
+  const items = faqItems.map((item) => ({
+    key: item.id,
+    label: item.question,
+    children: <p style={{ margin: 0 }}>{item.answer}</p>,
+    style: panelStyle,
+  }));
+
   return (
-    <div className={scss.Faq}>
+    <section className={style.faq}>
       <div className="container">
-        <div className={scss.content}>
-          <h2>FAQs</h2>
-
-          <ul className={scss.faqList}>
-            {faqItems.map((item, index) => {
-              const isOpen = openIndex === index;
-
-              return (
-                <li key={index} className={scss.faqItem}>
-                  <button
-                    type="button"
-                    className={`${scss.faqQuestion} ${isOpen ? scss.open : ""}`}
-                    onClick={() => toggle(index)}
-                  >
-                    <span>{item.question}</span>
-
-                    <span className={scss.icon}>
-                      <Image
-                        src={isOpen ? closeIcon : plusIcon}
-                        alt={isOpen ? "Close" : "Open"}
-                        width={20}
-                        height={20}
-                        className={scss.iconImage}
-                      />
-                    </span>
-                  </button>
-
-                  {isOpen && (
-                    <div className={scss.faqAnswer}>{item.answer}</div>
-                  )}
-                </li>
-              );
-            })}
-          </ul>
+        <div className={style.content}>
+          <h1 style={{ marginBottom: "40px" }}>FAQ</h1>
+          <Collapse
+            className={style.header}
+            bordered={false}
+            style={{ width: "100%" }}
+            expandIconPlacement="end"
+            expandIcon={({ isActive }) => (
+              <span className={style.icon}>
+                {isActive ? <CloseCircleOutlined /> : <PlusCircleOutlined />}
+              </span>
+            )}
+            items={items}
+          />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
